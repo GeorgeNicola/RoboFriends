8a84862a6c9008153c99ba9c193241d3c61518fa
GeorgeNicola <nicola_george1999@yahoo.com>
2020-06-25 18:55:41 +0300

    React + Redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,48 +1,53 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 import CardList from '../components/CardList.js';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
+import { setSearchField, requestRobots } from '../actions';
+
+
+const mapStateToProps = state =>{
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error,
+    }
+}//Returns the Redux state values
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots()),
+    }
+}//Changes the Redux State value
+
 
 
 class App extends Component {
-    constructor(){
-        super();
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-        //this.state reprezinta ce se schimba in aplicatie
-    }
-
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => [this.setState({ robots: users})]);
-    }
+        this.props.onRequestRobots();
+    }//Preia obiectul "robots" din Redux State
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value});
-    }
 
     render() {
-        const { robots, searchfield } = this.state; //Pentru a scrie mai putin
+        const { searchField, onSearchChange, robots, isPending } = this.props; //Props from Redux
 
         const filteredRobots = robots.filter( robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        });
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        });//Return a Array of robots that contain the searched name
 
-        if(!robots.length){
-            return <h1> Loading ... </h1>
-        }
-        else{
-            return(
+
+
+        return isPending ?
+             <h1> Loading ... </h1> : //Pand cand sunt preluate datele necesare, se afiseaza "Loading Screen"
+            (
                 <div className='tc'>
                     <h1> RoboFriends </h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
+                    <SearchBox searchChange={onSearchChange}/>
                     <Scroll>
                         <ErrorBoundry>
                             <CardList robots={filteredRobots} />
@@ -50,8 +55,9 @@ class App extends Component {
                     </Scroll>
                 </div>
             );
-        }    
+           
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+//Gives those 2 props to the App
